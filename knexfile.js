fdf68993eb04fbfda03076726be32bffc6478455
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2023-11-14 10:04:33 -0500

    update defaults to be safe

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASSWORD || 'postgres',
-      database: process.env.PG_DATABASE || 'postgres',
+      database: process.env.PG_DATABASE || 'congress_age_data',
     },
   },
   test: {
@@ -18,7 +18,7 @@ module.exports = {
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASSWORD || 'postgres',
-      database: process.env.PG_DATABASE || 'postgres',
+      database: process.env.PG_DATABASE || 'congress_age_data',
     },
   },
 };
